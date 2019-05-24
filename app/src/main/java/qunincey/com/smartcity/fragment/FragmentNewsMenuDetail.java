dba5ca866732f6257dc054c6125a5fd64d0bcc16
qunincey <408212480@qq.com>
2019-05-24 14:31:31 +0800

    第五天第一次

--- app/src/main/java/qunincey/com/smartcity/fragment/FragmentNewsMenuDetail.java
+++ app/src/main/java/qunincey/com/smartcity/fragment/FragmentNewsMenuDetail.java
@@ -9,6 +9,8 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,10 +20,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.viewpagerindicator.CirclePageIndicator;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
+import me.relex.circleindicator.CircleIndicator;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -36,6 +40,7 @@ import qunincey.com.smartcity.global.GlobalConstants;
 import qunincey.com.smartcity.utils.CacheUtils;
 import qunincey.com.smartcity.utils.MyBitmapUtils;
 import qunincey.com.smartcity.utils.OkhttpUtils;
+import qunincey.com.smartcity.utils.SpacesItemDecoration;
 
 public class FragmentNewsMenuDetail extends Fragment {
 
@@ -52,6 +57,10 @@ public class FragmentNewsMenuDetail extends Fragment {
     private Bundle bundle;
     private ViewPager viewPager;
     private NewsTabBean newsTabBean;
+    private TextView textView;
+    private CirclePageIndicator circleIndicator;
+    private RecyclerView recyclerView;
+    private MyBitmapUtils myBitmapUtils;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -75,14 +84,46 @@ public class FragmentNewsMenuDetail extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_menu_detail,container,false);
-
         return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        myBitmapUtils = new MyBitmapUtils();
         viewPager = view.findViewById(R.id.view_pager);
+
+        textView = view.findViewById(R.id.tv_topnews);
+        circleIndicator = view.findViewById(R.id.indicator);
+        recyclerView = view.findViewById(R.id.rv_list);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                textView.setText(newsTabBean.getData().topnews.get(i).title);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+        textView.setText(newsTabBean.getData().topnews.get(0).title);
+
+        /*
+        * 指定布局方式  recycview有多种布局方式
+        * */
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        NewsApdatar newsApdatar=new NewsApdatar(newsTabBean.getData().news);
+        recyclerView.setAdapter(newsApdatar);
+
     }
 
     @Override
@@ -95,11 +136,13 @@ public class FragmentNewsMenuDetail extends Fragment {
 
     private void initData() {
         viewPager.setAdapter(new imageAdpter());
+        circleIndicator.setViewPager(viewPager);
+        circleIndicator.setSnap(true);
     }
     /*初始化view*/
     private void initImageData(){
 
-        MyBitmapUtils myBitmapUtils=new MyBitmapUtils();
+
         imageViewsList=new ArrayList<>();
         for (int i=0;i<newsTabBean.data.topnews.size();i++){
             ImageView view=new ImageView(getActivity());
@@ -119,18 +162,6 @@ public class FragmentNewsMenuDetail extends Fragment {
          * 主线程不能开网络请求  会阻塞  用异步的
          *
          * */
-
-//        OkHttpClient client = new OkHttpClient();
-//        Request request=new Request.Builder().get().url(GlobalConstants.SERVER_URL+menu.getUrl()).build();
-//
-//        Call call = client.newCall(request);
-//        try {
-//            Response response=call.execute();
-//            String responseStr = response.body().string();
-//            newsTabBean = processNewsData(responseStr);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         OkhttpUtils.sendRequestWithOkhttp(GlobalConstants.SERVER_URL+menu.getUrl(), new Callback() {
 
             @Override
@@ -184,6 +215,52 @@ public class FragmentNewsMenuDetail extends Fragment {
         @Override
         public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
             container.removeView((View) object);
+        }
+    }
+
+    public  class NewsApdatar extends RecyclerView.Adapter<NewsApdatar.ViewHolder> {
+
+        ArrayList<NewsTabBean.NewsData> dataArrayList;
+
+        public NewsApdatar(ArrayList<NewsTabBean.NewsData> newsData) {
+            this.dataArrayList=newsData;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder{
+            private  ImageView imageView;
+            private final TextView textView;
+            private final TextView tv_data;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                imageView = itemView.findViewById(R.id.iv_icon);
+                textView = itemView.findViewById(R.id.tv_title);
+                tv_data = itemView.findViewById(R.id.tv_date);
+            }
+        }
+
+
+        @NonNull
+        @Override
+        public NewsApdatar.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+            View view=LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item_news,viewGroup,false);
+            ViewHolder viewHolder=new ViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+            NewsTabBean.NewsData newsData=dataArrayList.get(i);
+            myBitmapUtils.display(viewHolder.imageView,newsData.listimage);
+            viewHolder.textView.setText(newsData.title);
+            viewHolder.tv_data.setText(newsData.pubdate);
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return dataArrayList.size();
         }
     }
 }
